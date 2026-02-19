5e82292748d8715aca46fb5bc74f4adb7c2fca08
Taha Chadli <78033144+Taha-codes@users.noreply.github.com>
2026-02-19 11:06:07 +0000

    model

---
+++ tinystories-dense-m3/model.py
@@ -0,0 +1,11 @@
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+
+class CausalSelfAttention(nn.Module):
+    def __init__(self, n_embd, n_head, block_size, dropout):
+        super().__init__()
+        assert n_embd % n_head == 0
+        
+
+        
