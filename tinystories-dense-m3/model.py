75e2118ba11ebdb2a8c70f7b066bffc6be76e778
Taha Chadli <78033144+Taha-codes@users.noreply.github.com>
2026-04-06 17:36:46 +0100

    fixed the dimensions of embedding matrix

--- tinystories-dense-m3/model.py
+++ tinystories-dense-m3/model.py
@@ -5,7 +5,7 @@ import torch.nn as nn
 class TokenEmbedding(nn.Module):
     def __init__(self, vocab_size:int, d_model:int): #where we define the layers and parameters
         super().__init__()
-        self.embedding = nn.Embedding()
+        self.embedding = nn.Embedding(vocab_size, d_model)
         self.d_model = d_model
 
     def forward(self, x):
