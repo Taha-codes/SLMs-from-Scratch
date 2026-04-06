043e10aa7953f71b827a524f1e5fe65bb258dd6a
Taha Chadli <78033144+Taha-codes@users.noreply.github.com>
2026-04-06 17:35:20 +0100

    token embedding

--- tinystories-dense-m3/model.py
+++ tinystories-dense-m3/model.py
@@ -1,8 +1,14 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 
-from torch.optim import Adam
-from torch.utils.data import TensorDaataset, DataLoader
 
-import lightning as L 
+class TokenEmbedding(nn.Module):
+    def __init__(self, vocab_size:int, d_model:int): #where we define the layers and parameters
+        super().__init__()
+        self.embedding = nn.Embedding()
+        self.d_model = d_model
+
+    def forward(self, x):
+        return self.embedding(x)
+
+
