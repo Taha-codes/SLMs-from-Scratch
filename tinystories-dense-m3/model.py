485a4a208079869cb8737f2b7f0ce202c9cc0a9d
Taha Chadli <78033144+Taha-codes@users.noreply.github.com>
2026-04-07 12:12:32 +0100

    Multi-head attention

--- tinystories-dense-m3/model.py
+++ tinystories-dense-m3/model.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-
+import math
 
 class TokenEmbedding(nn.Module):
     def __init__(self, vocab_size:int, d_model:int): #where we define the layers and parameters
@@ -14,6 +14,7 @@ class TokenEmbedding(nn.Module):
 class PositionalEncoding(nn.Module):
     def __init__(self, d_model:int, seq_len:int, dropout: float):
         super().__init__()
+
         self.dropout = nn.Dropout(dropout)
 
         #pre conputing the positional matrix
@@ -23,11 +24,62 @@ class PositionalEncoding(nn.Module):
         div_term  = torch.exp(torch.arange(0, d_model, 2) * (-math.log(10000.0) / d_model))    # (d_model/2,)
         pe[:, 0::2] = torch.sin(positions * div_term)  # even dims
         pe[:, 1::2] = torch.cos(positions * div_term)  # odd dims
-        pe = pe.unsqueeze(0)  # (1, seq_len, d_model) ← add batch dim
+        pe = pe.unsqueeze(0)  # (1, seq_len, d_model) because we need to add the batch dim
 
         self.register_buffer('pe', pe)  # fixed, not a learned parameter
     
     def forward(x, self):
         x = x + self.pe[:, :x.shape[1], :]  # add position to each token
         return self.dropout(x)
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, d_model, n_heads, dropout):
+        super().__init__()
+
+        self.W_q = nn.Linear(d_model, d_model)
+        self.W_k = nn.Linear(d_model, d_model)
+        self.W_v = nn.Linear(d_model, d_model)
+        self.W_o = nn.Linear(d_model, d_model)
+        self.n_heads = n_heads
+
+        assert d_model % n_heads == 0, "embedding dimension not divisible by number of heads"
+
+        self.d_k = d_model // n_heads
+
+    def forward(self, x, mask = None):
+
+        B, T, _ = x.shape
+
+        # first we project the input matrix X into the Q, K and V matrices
+        # the input matrix X is (betch, seq_len, d_model)
+        Q = self.W_q(x)
+        K = self.W_k(x)
+        V = self.W_v(x)
+
+        # we split into h heads and swap the seq_len and n_heads dimensions for matrix multiply
+        Q = Q.view(B, T, self.n_heads, self.d_k).transpose(1, 2)
+        K = K.view(B, T, self.n_heads, self.d_k).transpose(1, 2)
+        V = V.view(B, T, self.n_heads, self.d_k).transpose(1, 2)
+
+        # calculate the scores
+        scores = (Q @ K.transpose(-2, -1) / math.sqrt(self.d_k))
+
+        # Apply mask if training
+        if mask is not None:
+            scores = scores.masked_fill(mask == 0, float('-inf'))
+
+        # softmax the scores to get the attention weights
+        weights = torch.softmax(scores, dim=-1)
+
+        # weighteed sum
+        x = weights @ V
+
+        #concatenate heads back together for W_o multiply
+        x = x.transpose(1,2).contiguous().view(B, T, self.n_heads * self.d_k)
+        
+        return self.W_o(x)
+
+
+
+
 
