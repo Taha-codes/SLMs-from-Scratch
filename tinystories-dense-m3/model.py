74b19f85b1162ebb2a83f9db02b487c49d50e1e2
Taha Chadli <78033144+Taha-codes@users.noreply.github.com>
2026-04-07 10:35:15 +0100

    positional encoding is driving me crazy

--- tinystories-dense-m3/model.py
+++ tinystories-dense-m3/model.py
@@ -11,4 +11,23 @@ class TokenEmbedding(nn.Module):
     def forward(self, x):
         return self.embedding(x)
 
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model:int, seq_len:int, dropout: float):
+        super().__init__()
+        self.dropout = nn.Dropout(dropout)
+
+        #pre conputing the positional matrix
+        pe = torch.zeros(seq_len, d_model)
+
+        positions = torch.arange(0, seq_len).unsqueeze(1)        # (seq_len, 1)
+        div_term  = torch.exp(torch.arange(0, d_model, 2) * (-math.log(10000.0) / d_model))    # (d_model/2,)
+        pe[:, 0::2] = torch.sin(positions * div_term)  # even dims
+        pe[:, 1::2] = torch.cos(positions * div_term)  # odd dims
+        pe = pe.unsqueeze(0)  # (1, seq_len, d_model) ← add batch dim
+
+        self.register_buffer('pe', pe)  # fixed, not a learned parameter
+    
+    def forward(x, self):
+        x = x + self.pe[:, :x.shape[1], :]  # add position to each token
+        return self.dropout(x)
 
